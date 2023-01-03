a04250a964095772b04833af62b47bc40f58dd43
Emil1577 <emilronquillo@gmail.com>
2023-01-03 14:12:38 -0800

    words are created

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,17 @@
 var startGame = document.querySelector("#start");
 var timer = document.querySelector("#score");
+var word = document.querySelector("#guess-word");
 
 
-
+//Triggers to start the game
 startGame.addEventListener("click", timerStarts);
 
 
+//variable that sets the timer
 var secondsLeft = 50;
 
+
+// function for timer
 function timerStarts() {
 
     var timerInterval = setInterval (function() {
@@ -21,7 +25,34 @@ function timerStarts() {
     },1000);
 }
 
- // startTimer.textContent = "Time Reduced";
-function deductTime () {
-    secondsLeft = secondsLeft - 10;
-} 
+
+
+    // startTimer.textContent = "Time Reduced";
+    //function deductTime () {
+    //  secondsLeft = secondsLeft - 10;
+    //} 
+
+/// create a function to start the game
+    // starts the timer function
+    // starts random word function
+
+/// create function for random word
+    // pull random words from array of strings
+    // rendering the word on the screen
+
+// rendering word
+
+// function to keep scores in storage
+
+
+const wordList = [
+    
+    "Javascript",
+    "Eagle",
+    "warriors",
+    "BMW",
+    "hello World"
+]
+
+
+
