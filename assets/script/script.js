7439f8e463dd8827359915038dc2e7dbee692e9e
Emil1577 <emilronquillo@gmail.com>
2023-01-03 13:41:43 -0800

    initial CSS JS and HTML

---
+++ assets/script/script.js
@@ -0,0 +1,27 @@
+var startGame = document.querySelector("#start");
+var timer = 
+
+
+
+startGame.addEventListener("click", timerStarts);
+
+
+var secondsLeft = 50;
+
+function timerStarts() {
+
+    var timerInterval = setInterval (function() {
+        secondsLeft--;
+        timer.textContent = secondsLeft + " left";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert = "GAME ENDS";
+        }
+    },1000);
+}
+
+ // startTimer.textContent = "Time Reduced";
+function deductTime () {
+    secondsLeft = secondsLeft - 10;
+} 
